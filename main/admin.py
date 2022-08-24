6cf6dad137e2ec284dfb1acaf2090ec9625ebb62
showq <shoooq-00123@hotmail.com>
2022-08-24 17:08:25 +0300

    Admin and Apps

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import tutorial 
+from tinymce.widgets import TinyMCE
+from django.db import models
 
 # Register your models here.
+
+class tutorialAdmin(admin.ModelAdmin):
+    fieldsets = [ 
+        ("title date" , {"fields": ["tutorialTitle","tutorialPublished"]}),
+        ("content" , {"fields": ["tutorialContent"]})
+    ]
+    formfield_overrides={
+        models.TextField:{'widget' : TinyMCE()}
+    }
+    
+''' fields=["tutorialTitle",
+            "tutorialPublished",
+            "tutorialContent"
+            ]'''
+       
+    
+admin.site.register(tutorial ,tutorialAdmin )
--- main/models.py
+++ main/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
 class tutorial (models.Model):
     tutorialTitle = models.CharField(max_length=200)#Required. The maximum length (in characters) of the field.
-    tutorialContent =models.TextField("")
-    tutorialPublished = models.DateTimeField("date published ")
-    
+    tutorialContent = models.TextField("tutorialContent")
+    tutorialPublished = models.DateTimeField("date published",default=datetime.now())    
     #we can make various overwrite 
     def __str__(self):
         return self.tutorialTitle
