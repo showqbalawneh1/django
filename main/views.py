4566a0557c24d151c16105e0f17e2f1fcc4dc8ba
showq <shoooq-00123@hotmail.com>
2022-08-24 23:08:58 +0300

    UserRegesteration

--- main/views.py
+++ main/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse 
 from .models import tutorial
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login ,logout ,authenticate
+#authenticate to the enter password and other password 
 # Create your views here.
 
 
@@ -9,6 +12,22 @@ def homepage(request):
 	template_name="main/home.html",
 	context={"tutorials":tutorial.objects.all})
 
-
+def register(request):
+    if request.method=="POST":
+        form =UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("main:homepage")
+        else:
+            for msg in form.error_messages:
+                print(form.error_messages[msg])
+                return render(request = request,
+                template_name = "main/register.html",
+                context={"form":form})
+    else:
+        form = UserCreationForm
+        return render(request, "main/register.html",context={"form":form}) 
+    
 '''def homepage(request):
     return HttpResponse ("hi hi ")'''
