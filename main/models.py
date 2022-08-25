fb0eb09a3896bee00e48314e8d54a7f04be77707
showq <shoooq-00123@hotmail.com>
2022-08-25 19:03:44 +0300

    foreign key with model done

--- main/models.py
+++ main/models.py
@@ -15,7 +15,7 @@ class tutorialCategory(models.Model):
     
 class tutorialSeries(models.Model):
     seriesTutorial = models.CharField(max_length=200)
-    categoryTutorial = models.ForeignKey(tutorialCategory,verbose_name="Category", on_delete=models.CASCADE)
+    categoryTutorial = models.ForeignKey(tutorialCategory,default=None, on_delete=models.SET_DEFAULT)
     seriesSummary = models.CharField(max_length=200)
     class Meta:
         verbose_name_plural = "Series"
@@ -27,8 +27,8 @@ class tutorial (models.Model):
     tutorialTitle = models.CharField(max_length=200)#Required. The maximum length (in characters) of the field.
     tutorialContent = models.TextField("tutorialContent")
     tutorialPublished = models.DateTimeField("date published",default=datetime.now())
-    #seriesTutorial = models.ForeignKey(tutorialSeries, verbose_name="Series", on_delete=models.CASCADE)
-    #tutorialSlug = models.CharField(max_length=200, default=1)
+    seriesTutorial = models.ForeignKey(tutorialSeries, default=None, verbose_name="Series", on_delete=models.SET_DEFAULT)
+    tutorialSlug = models.CharField(max_length=200, default=1)
     def __str__(self):
         return self.tutorialTitle
     
