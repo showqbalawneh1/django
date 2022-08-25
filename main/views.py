41e369a1b7166d6b3b353539d19878d8f04a2965
showq <shoooq-00123@hotmail.com>
2022-08-25 12:44:01 +0300

    message and includes

--- main/views.py
+++ main/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import tutorial
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login ,logout ,authenticate
+from django.contrib import messages
 #authenticate to the enter password and other password 
 # Create your views here.
 
@@ -13,21 +14,26 @@ def homepage(request):
 	context={"tutorials":tutorial.objects.all})
 
 def register(request):
-    if request.method=="POST":
-        form =UserCreationForm(request.POST)
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f"New account created: {username}")
             login(request, user)
             return redirect("main:homepage")
+
         else:
             for msg in form.error_messages:
-                print(form.error_messages[msg])
-                return render(request = request,
-                template_name = "main/register.html",
-                context={"form":form})
-    else:
-        form = UserCreationForm
-        return render(request, "main/register.html",context={"form":form}) 
-    
+                messages.error(request, f"{msg}: {form.error_messages[msg]}")
+
+            return render(request = request,
+                          template_name = "main/register.html",
+                          context={"form":form})
+
+    form = UserCreationForm
+    return render(request = request,
+                  template_name = "main/register.html",
+                  context={"form":form})
 '''def homepage(request):
     return HttpResponse ("hi hi ")'''
